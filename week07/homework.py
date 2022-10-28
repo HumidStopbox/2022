1081811eebeebc714c77adb2f8eda6a3cfa3b969
jsamg6@gmail.com <jsamg6@gmail.com>
2022-10-28 10:15:26 -0400

    week 07

---
+++ week07/homework.py
@@ -0,0 +1,12 @@
+for m in range(0,10):
+    print("0", end=" ")
+
+for m in range(0,10):
+    for n in range(m,10):
+        print(" ", end=" ")
+    print("0")
+
+for m in range(0,10):
+    print("0", end=" ")
+
+print("I'm not sure why there's one 0 up there haha")
