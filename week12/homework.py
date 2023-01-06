c6de6916dff7ea0a8e89cc7263d470d71876b843
jsamg6@gmail.com <jsamg6@gmail.com>
2023-01-05 20:51:21 -0500

    homework

---
+++ week12/homework.py
@@ -0,0 +1,129 @@
+def items(list, prefix):
+    for item in list:
+        if item.startswith(prefix):
+            yield item + "??"
+
+list = ["apricot", "cyan", "black", "blue", "bluetiful",
+    "blue green", "blue violet", "brown", "carnation pink",
+    "cerulean", "gray", "green", "green yellow", "indigo",
+    "orange", "red", "red orange", "red violet", "scarlet",
+    "violet", "purple", "violet red", "white", "yellow",
+    "yellow green", "yellow orange", "brown", "blue",
+    "green", "pink", "daffodil", "emerald green",
+    "eggplant purple", "forest green", "french rose", "fuchsia",
+    "hazel", "indigo", "ice clear", "jade", "key lime pie", "lime",
+    "lilac", "maple", "maroon", "mango yellow", "mango yellow",
+    "magnolia", "navy blue", "quartz grey", "queen blue",
+    "teal", "taupe", "ultra violet", "ultramarine", "xanthic",
+    "xanadu", "yapan", "zinc grey", "zinger", "zombie green",
+    "zucchini green", "zesty orange", "zesty red", "jazzamoon",
+    "dairy Cream", "school bus yellow", "sea green", "seashell"]
+
+f = "were you searching for the color"
+
+
+answer = input("Enter letter to search dictionary:")
+if answer == "a":
+    print("were you searching for one of these colors:")
+    for item in items(list, "a"):
+        print(item)
+elif answer == "b":
+    print("were you searching for one of these colors:")
+    for item in items(list, "b"):
+        print(item)
+elif answer == ("c"):
+    print("were you searching for one of these colors:")
+    for item in items(list, "c"):
+        print(item)
+if answer == "d":
+    print("were you searching for one of these colors:")
+    for item in items(list, "d"):
+        print(item)
+elif answer == "e":
+    print("were you searching for one of these colors:")
+    for item in items(list, "e"):
+        print(item)
+elif answer == ("f"):
+    print("were you searching for one of these colors:")
+    for item in items(list, "f"):
+        print(item)
+elif answer == "g":
+    print("were you searching for one of these colors:")
+    for item in items(list, "g"):
+        print(item)
+elif answer == ("h"):
+    print("were you searching for one of these colors:")
+    for item in items(list, "h"):
+        print(item)
+if answer == "i":
+    print("were you searching for one of these colors:")
+    for item in items(list, "i"):
+        print(item)
+elif answer == "j":
+    print("were you searching for one of these colors:")
+    for item in items(list, "j"):
+        print(item)
+elif answer == ("k"):
+    print("were you searching for one of these colors:")
+    for item in items(list, "k"):
+        print(item)
+elif answer == "l":
+    print("were you searching for one of these colors:")
+    for item in items(list, "l"):
+        print(item)
+elif answer == ("m"):
+    print("were you searching for one of these colors:")
+    for item in items(list, "m"):
+        print(item)
+if answer == "n":
+    print("were you searching for one of these colors:")
+    for item in items(list, "n"):
+        print(item)
+elif answer == "o":
+    print("were you searching for one of these colors:")
+    for item in items(list, "o"):
+        print(item)
+elif answer == ("p"):
+    print("were you searching for one of these colors:")
+    for item in items(list, "p"):
+        print(item)
+elif answer == "q":
+    print("were you searching for one of these colors:")
+    for item in items(list, "q"):
+        print(item)
+elif answer == ("r"):
+    print("were you searching for one of these colors:")
+    for item in items(list, "r"):
+        print(item)
+if answer == "s":
+    print("were you searching for one of these colors:")
+    for item in items(list, "s"):
+        print(item)
+elif answer == "t":
+    print("were you searching for one of these colors:")
+    for item in items(list, "t"):
+        print(item)
+elif answer == ("u"):
+    print("were you searching for one of these colors:")
+    for item in items(list, "u"):
+        print(item)
+elif answer == "v":
+    print("were you searching for one of these colors:")
+    for item in items(list, "v"):
+        print(item)
+elif answer == ("w"):
+    print("were you searching for one of these colors:")
+    for item in items(list, "w"):
+        print(item)
+if answer == "x":
+    print("were you searching for one of these colors:")
+    for item in items(list, "x"):
+        print(item)
+elif answer == "y":
+    print("were you searching for one of these colors:")
+    for item in items(list, "y"):
+        print(item)
+elif answer == ("z"):
+    print("were you searching for one of these colors:")
+    for item in items(list, "z"):
+        print(item)
