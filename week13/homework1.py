050874af3cc19514d855573bc2defb665f2e8226
jsamg6@gmail.com <jsamg6@gmail.com>
2023-01-17 13:04:40 -0500

    homework week 13

---
+++ week13/homework1.py
@@ -0,0 +1,49 @@
+for m in range(0,5):
+    print("F", end=" ")
+for m in range(0,1):
+    for n in range(m,3):
+        print("F")
+for m in range(0,3):
+    print("F", end=" ")
+for m in range(0,1):
+    for n in range(m,4):
+        print("F")
+print("           ")
+
+for m in range(0,1):
+    print(" ", end=" ")
+for m in range(0,5):
+    print("I", end=" ")
+for m in range(0,1):
+    for n in range(m,1):
+        print("       ")
+for m in range(0,1):
+    for n in range(m,4):
+        print("      I")
+for m in range(0,1):
+    print(" ", end=" ")
+for m in range(0,5):
+    print("I", end=" ")
+print("           ")
+
+print("           ")
+print("N         N")
+print("N N       N")
+print("N   N     N")
+print("N     N   N")
+print("N       N N")
+print("N         N")
+
+print("     A     ")
+print("    A A    ")
+print("   A   A   ")
+print("  A A A A  ")
+print(" A       A ")
+print("A         A")
+
+print("           ")
+for m in range(0,1):
+    for n in range(m,6):
+        print("L")
+for m in range(0,5):
+    print("L", end=" ")
